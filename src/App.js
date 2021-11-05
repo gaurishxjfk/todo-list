b2310f322ccb916f918f3107e8a9728601d78c0c
Gaurish Naik <nerdgaurish@gmail.com>
2021-11-05 17:04:10 +0530

    changes in styles

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import { useState } from 'react';
 import './App.css';
-import axios from 'axios'
-import { userData } from './config/data';
 import Login from './components/Login';
 import Addtask from './components/Addtask';
 import {  BrowserRouter as Router,  Switch,  Route,  Redirect} from "react-router-dom";
 import { UserState } from './context';
-import {Button, Container} from '@mui/material';
-
 
 function App() {
 
-  const { isLoggedIn,setIsLoggedIn,username,password } = UserState();
-
-  var userArr = userData.map(i => i.username);
-  var passArr = userData.map(i => i.password);
-
-  const [users, setUsers] = useState();
-  
-  
-
-const logout = () => {
-  setIsLoggedIn(false)
-}
+  const { isLoggedIn } = UserState();
   
   return (
     <Router className="App">
